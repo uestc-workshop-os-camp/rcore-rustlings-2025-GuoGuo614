317c6ce529552cd711c6076126881527bbd60cc0
GuoGuo <2251270969@qq.com>
2025-09-07 14:41:13 +0800

    hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -34,11 +34,28 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
+        // Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name)
+            .and_modify(|team| {
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        let team_2 = scores.entry(team_2_name)
+            .or_insert(Team {
+                goals_scored: 0, 
+                goals_conceded: 0
+            });
+        (*team_2).goals_scored += team_2_score;
+        (*team_2).goals_conceded += team_1_score;
     }
     scores
 }
