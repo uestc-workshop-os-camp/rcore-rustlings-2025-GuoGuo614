b45cdabe09ce41b3c07e84e27cc04c9cfa31a58f
GuoGuo <2251270969@qq.com>
2025-09-24 08:51:33 +0800

    conversations #2

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard<T> {
     pub grade: T,
     pub student_name: String,
